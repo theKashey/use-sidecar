87197c6880c1467730ab24eb3c4b7564fca820ef
Anton Korzunov <thekashey@gmail.com>
2022-04-17 18:11:53 +1000

    update infrastructure

---
+++ .size-limit.js
@@ -0,0 +1,7 @@
+module.exports = [
+  {
+    path: 'dist/es2015/index.js',
+    size: '1.4kb',
+    limit: '100 ms',
+  },
+];
--- src/renderProp.tsx
+++ src/renderProp.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {useState, useCallback, useEffect, useLayoutEffect} from "react";
+import { useState, useCallback, useEffect, useLayoutEffect } from 'react';
 
 type CombinedProps<T extends any[], K> = { children: (...prop: T) => any } & K;
 type RenderPropComponent<T extends any[], K> = React.ComponentType<CombinedProps<T, K>>;
@@ -16,42 +16,45 @@ type ChildrenProps<T extends any[], K> = {
   children: (...prop: T) => any;
 };
 
-export function renderCar<T extends any[], K>(WrappedComponent: RenderPropComponent<T, K>, defaults: (props: K) => T, options: Options = {}) {
-  function State({stateRef, props}: { stateRef: React.RefObject<Callback>, props: CombinedProps<T, K> }) {
+export function renderCar<T extends any[], K>(
+  WrappedComponent: RenderPropComponent<T, K>,
+  defaults: (props: K) => T,
+  options: Options = {}
+) {
+  function State({ stateRef, props }: { stateRef: React.RefObject<Callback>; props: CombinedProps<T, K> }) {
     const renderTarget = useCallback(function SideTarget(...args: T) {
-        useLayoutEffect(() => {
-          stateRef.current(args)
-        });
+      useLayoutEffect(() => {
+        stateRef.current(args);
+      });
 
-        return null;
-      },
-      []);
-
-    return <WrappedComponent {...props} children={renderTarget}/>;
-  }
-
-  const Children = React.memo(({stateRef, defaultState, children}: ChildrenProps<T, K>) => {
-    const [state, setState] = useState<T>(defaultState.current);
-    React.useEffect(() => {
-      stateRef.current = setState;
+      return null;
     }, []);
 
-    return children(...state);
-  }, () => true);
+    return <WrappedComponent {...props} children={renderTarget} />;
+  }
+
+  const Children = React.memo(
+    ({ stateRef, defaultState, children }: ChildrenProps<T, K>) => {
+      const [state, setState] = useState<T>(defaultState.current);
+
+      useEffect(() => {
+        stateRef.current = setState;
+      }, []);
+
+      return children(...state);
+    },
+    () => true
+  );
 
   return function Combiner(props: CombinedProps<T, K>) {
     const defaultState = React.useRef<T>(defaults(props));
-    const ref = React.useRef((state:T) => defaultState.current = state);
+    const ref = React.useRef((state: T) => (defaultState.current = state));
 
     return (
       <React.Fragment>
-        <State stateRef={ref} props={props}/>
-        <Children
-          stateRef={ref}
-          defaultState={defaultState}
-          children={props.children}
-        />
+        <State stateRef={ref} props={props} />
+        <Children stateRef={ref} defaultState={defaultState} children={props.children} />
       </React.Fragment>
     );
-  }
+  };
 }
